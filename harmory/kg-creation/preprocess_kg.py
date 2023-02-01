c5cf78ddc6ea21f1683a2f512ca634a2b3905b53
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2023-02-01 02:54:41 +0100

    add similarity processing for KG population

--- harmory/kg-creation/preprocess_kg.py
+++ harmory/kg-creation/preprocess_kg.py
@@ -7,12 +7,13 @@ import sys
 from pathlib import Path
 
 import joblib
+import numpy as np
 import pandas as pd
 
 sys.path.append(os.path.dirname(os.path.dirname('../../harmory')))
 
 
-class PreprocessData:
+class PreprocessTrack:
     """
     Class for preprocessing track data and similarity data for the creation of
     the Harmory KG.
@@ -158,6 +159,114 @@ class PreprocessData:
         return similarities
 
 
+class PreprocessSimilarity:
+    """
+    Class to preprocess the similarity data.
+    """
+
+    def __init__(self,
+                 dataset_path: str | Path,
+                 id_map_path: str | Path,
+                 similarity_path: str | Path):
+        """
+        Constructor for the PreprocessSimilarity class.
+        Parameters
+        ----------
+        id_map_path : str | Path
+            Path to the id map file
+        similarity_path : str | Path
+            Path to the similarity file
+        """
+        if isinstance(dataset_path, str):
+            dataset_path = Path(dataset_path)
+        if isinstance(id_map_path, str):
+            id_map_path = Path(id_map_path)
+        if isinstance(similarity_path, str):
+            similarity_path = Path(similarity_path)
+
+        assert dataset_path.exists(), f"Path {dataset_path} does not exist"
+        assert dataset_path.is_dir(), f"Path {dataset_path} is not a directory"
+        assert id_map_path.exists(), f"Path {id_map_path} does not exist"
+        assert id_map_path.is_file(), f"Path {id_map_path} is not a file"
+        assert similarity_path.exists(), f"Path {similarity_path} does not exist"
+        assert similarity_path.is_file(), f"Path {similarity_path} is not a file"
+
+        self._dataset_path = dataset_path
+        self._id_map_path = id_map_path
+        self._similarity_path = similarity_path
+
+        self.id_map_data = joblib.load(self._id_map_path)
+        # self.id_map_data = {v: k for k, v in id_map_data.items()}
+        self.similarity_data = pd.read_csv(self._similarity_path, sep=',')
+        self._file_names = [x.stem for x in self._dataset_path.glob('*')]
+
+    def get_sequence(self, pattern_id: int) -> np.ndarray:
+        """
+        Get the sequence of segments for a given track.
+        Parameters
+        ----------
+        pattern_id : int
+            Index of the sequence to search
+        Returns
+        -------
+        list
+            Time series of the segment
+        """
+        assert pattern_id in self.id_map_data.keys(), \
+            f"Pattern id {pattern_id} not in map data"
+        file = self.id_map_data[pattern_id].split('_')
+        file_name, sequence_idx = '_'.join(file[:-1]), int(file[-1])
+        assert file_name in self._file_names, \
+            f"File name {file_name} not in file names"
+        track_data = joblib.load(self._dataset_path / f'{file_name}.pickle')
+
+        return track_data[sequence_idx].time_series
+
+    def get_sequence_string(self, pattern_id: int) -> str:
+        """
+        Get the sequence of segments for a given track.
+        Parameters
+        ----------
+        pattern_id : int
+            Index of the sequence to search
+        Returns
+        -------
+        list
+            Time series of the segment
+        """
+        assert pattern_id in self.id_map_data.keys(), \
+            f"Pattern id {pattern_id} not in map data"
+        file = self.id_map_data[pattern_id].split('_')
+        file_name, sequence_idx = '_'.join(file[:-1]), int(file[-1])
+        assert file_name in self._file_names, \
+            f"File name {file_name} not in file names"
+        track_data = joblib.load(self._dataset_path / f'{file_name}.pickle')
+
+        return '_'.join([str(x) for x in track_data[sequence_idx].time_series])
+
+    def get_similarities(self, pattern_id: int) -> list[tuple[int, float]]:
+        """
+        Get the similarities for a given track.
+        Parameters
+        ----------
+        pattern_id : int
+            Index of the sequence to search
+        Returns
+        -------
+        list[tuple[int, float]]
+            List of tuples with the pattern id and the distance
+        """
+        assert pattern_id in self.id_map_data.keys(), \
+            f"Pattern id {pattern_id} not in map data"
+        segment_id = self.id_map_data[pattern_id]
+        # take all the similarities where the source or the target is the segment id
+        similarities = []
+        if pattern_id in self.similarity_data.source:
+            print('source')
+
+        return similarities
+
+
 if __name__ == '__main__':
     # abc = joblib.load('../../data/structures/small-audio/billboard_12.pkl')
     # print(abc[0].time_series)
@@ -165,9 +274,14 @@ if __name__ == '__main__':
     # cde = joblib.load('../../data/similarities/pattern2id.pkl')
     # print(cde)
 
-    pre = PreprocessData(
+    pre = PreprocessTrack(
         '../../data/structures/small-billboard/billboard_5.pickle',
         '../../data/similarities/pattern2id.pkl',
         '../../data/similarities/similarities.csv')
     pre.get_metadata('../../data/metadata/meta.csv')
     print(pre.get_sequence_string(3))
+
+    sim = PreprocessSimilarity('../../data/structures/small-billboard',
+                               '../../data/similarities/pattern2id.pkl',
+                               '../../data/similarities/similarities.csv')
+    print(sim.get_similarities(1457))
